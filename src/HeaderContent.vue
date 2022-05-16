76a4f7d7c27caad7072a27afe76fc3aad34678ca
Giulia-92 <CIONCO.GIULIA@GMAIL.COM>
2022-05-16 12:17:33 +0200

    proj-html-vuejs

---
+++ src/HeaderContent.vue
@@ -0,0 +1,28 @@
+<template>
+    
+<header>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</header>
+    
+</template>
+<script>
+export default {
+  name: 'HeaderContent',
+  data(){
+    return{
+    
+    }
+  }
+}
+
+    
+</script>
+<style>
+
+</style>
---
+++ src/MainContent.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    
+</div>
+    
+</template>
+
+<script>
+export default {
+  name: 'MainContent',
+  data(){
+    return{
+    }
+  }
+} 
+</script>
+<style>
+
+</style>

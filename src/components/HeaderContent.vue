6db110b3589fb633c3df20d6a19dc5f4f0c498e0
Giulia-92 <CIONCO.GIULIA@GMAIL.COM>
2022-05-16 21:33:11 +0200

    proj-html-vuejs

--- src/components/HeaderContent.vue
+++ src/components/HeaderContent.vue
@@ -1,26 +1,26 @@
 <template>
     
 <header>
-  <nav class="navbar bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-      <img src="./img/logo-sidearea-1-1.png" alt="" width="30" height="24">
-    </a>
-  </div>
-</nav>
-    <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
-    <h1>Devition that never ends</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, tempore! A, minima. Adipisci, rem esse! Quaerat modi deserunt ullam id et soluta doloribus, qui corrupti impedit at, minima hic saepe.</p>
-    <div class="picture">
-    <img src="./assets/img/h-2-slider-img-15.png" alt="figura1">
-    <img src="./assets/img/h-2-slider-img-16.png" alt="figura2">
+  <img class="logo" src="../assets/img/logo-sidearea-1-1.png" alt="">
+  <div class="top1">
+    <h1>Devotion that never <h2>ends</h2></h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Adipisci, rem esse! Quaerat modi deserunt ullam id et soluta doloribus, qui corrupti impedit at, minima hic saepe.</p>
+   <button class="btn1">READ MORE</button>
     </div>
+    <div class="picture">
+    <img src="../assets/img/h-2-slider-img-15.png" alt="figura1">
+    <img src="../assets/img/h-2-slider-img-16.png" alt="figura2">
+    </div>
+  
+  <section>
+   <img src="../assets/img/h-2-port-img-1.jpg" class="img-fluid" alt="...">
+   <img src="../assets/img/h-2-port-img-2.jpg" class="img-fluid" alt="...">
+   <img src="../assets/img/h-2-port-img-3.jpg" class="img-fluid" alt="...">
+   <img src="../assets/img/h-2-port-img-4.jpg" class="img-fluid" alt="...">
+   <img src="../assets/img/h-2-port-img-5.jpg" class="img-fluid" alt="...">
+   <img src="../assets/img/h-2-port-img-6.jpg" class="img-fluid" alt="...">
+  </section>
+   
 </header>
     
 </template>
@@ -37,10 +37,42 @@ export default {
     
 </script>
 <style>
-h1{
-  font: size 20px;
+.top1{
+  font-size:20px;
   color: black;
   font-family:'Montserrat', sans-serif;
+  margin-left:25%;
+  margin-top:15%;
+  margin-right:50%;
+}
+p{
+  color: #A6A6A6;
+  font-size:13px;
+}
+h2{
+  color:black;
+ font-family: 'Lobster', cursive;
+}
+.btn1{
+  color: black;
+  font-size:10px;
+  width:120px;
+  height:40px;
+  border:2px solid #E1C0B0;
+  font-family:'Montserrat', sans-serif;
+  background-color:#FFFFFF;
+}
+
+.picture{
+  margin-top:-15%;
+  display: flex;
+  justify-content: flex-end;
+}
+.img-fluid{
+
+width:550px;
+display:inline-flex;
+
 }
 
 </style>
